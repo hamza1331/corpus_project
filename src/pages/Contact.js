dea1959e3dfb6ed7fce3303dc0aeb26e7ca2c174
abrehman-odoo <abrehman_odoo@thinksolutionz.org>
2022-07-16 00:27:39 +0500

    new changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,6 +5,8 @@ export default function Contact() {
   return (
     <div>
       <Bar/>
+      <br/>
+      <br/>
       <div className="container border border-success border-2 p-4 pt-100 pb-70 text-center ">
         <div className='container '>
         <br/>
@@ -18,52 +20,28 @@ export default function Contact() {
                 </p>
                 </div>
                 <br/>
+                <br/>
         </div>
                 <div className="container">
-                    <div className="row py-3">
-
-                        <div className="col-lg-4 col-sm-6">
-                            <div className="card p-2">
-                            <i class="fa fa-map-marker fa-lg" aria-hidden="true"></i>
-                                <h3>Location</h3>
-                                <p>VISIT TO EXPLORE THE WORLD</p>
-                                <br/>
-                                <h6>264-A main college road UET</h6>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-4 col-sm-6">
-                            <div className="card p-2">
+                    <div className="row py-4">
+                        <div className="col-lg-6 col-sm-8">
+                            <div className="card p-1 py-3">
                             <i class="fa fa-phone fa-lg" aria-hidden="true"></i>
                                 <h3>Make a Call</h3>
-                                <p>LET'S TALK WITH OUR EXPERTS</p>
+                                <p>Emails 📧 or message with querry in subject would be appreciated.</p>
                                 <br/>
-                                <h6>04232326502</h6>
+                                <h6>(+92) 3084432015</h6>
                             </div>
                         </div>
 
-                        <div className="col-lg-4 col-sm-6">
-                            <div className="card p-2">
+                        <div className="col-lg-6 col-sm-8">
+                            <div className="card p-1 py-3">
                             <i class="fa fa-envelope fa-lg" aria-hidden="true"></i>
                                 <h3>Send a Mail</h3>
                                 <p>DON'T HESITATE TO MAIL</p>
                                 <br/>
-                                <h6>info@odoo-ai.com</h6>
+                                <h6>Fatematuzzehra@gmail.com</h6>
                             </div>
-                        </div>
-
-                    </div>
-                    
-                    <br/>
-
-                    <div className="row">
-                        <div>
-                            <div className="about-img">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3405.156289277303!2d74.26957801514
-                            793!3d31.40981978140627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa76ccca835a19297!2z
-                            MzHCsDI0JzM1LjQiTiA3NMKwMTYnMTguNCJF!5e0!3m2!1sen!2s!4v1648394855547!5m2!1sen!2s" width="980" height="400"  allowfullscreen="" ></iframe>
-                            </div>
-                            <br/>
                         </div>
 
                     </div>
