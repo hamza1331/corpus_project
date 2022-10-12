c974aac92df4a2b259419010d72e3332e70fb462
Shaggy <hamxa1331u@gmail.com>
2022-10-12 18:22:38 +0500

    ud words

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,6 +1,6 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Bar from '../components/Bar'
-import { useNavigate,useLocation } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { url } from "../components/Variable";
 export default function List() {
   const loction = useLocation();
@@ -8,132 +8,196 @@ export default function List() {
   const navigation = useNavigate();
   const [Word, setWord] = useState('');
   const [data, setData] = useState(null)
-  const [wordDescription,setWordDescription] = useState('')
-  const [showConcordance,setShowConcordance] = useState(false)
+  const [wordDescription, setWordDescription] = useState('')
+  const [showConcordance, setShowConcordance] = useState(false)
   const [concordanceResults, setconcordanceResults] = useState([])
+  const [descriptions, setdescriptions] = useState([])
   const searchConcordance = () => {
     // console.log('abcccc-->',abc)
-      if (abc.length > 0) {
-        fetch(`${url}/corpus/findKWIC`, {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-          },
-          body: JSON.stringify({
-            word: abc,
-            criteria: 'all'
-          })
-        }).then(res => res.json())
-          .then((response) => {
-  
-            // console.log('Data received search word --->', response);
-            // after
-            if (response.message === 'Success') {
-              // setIsLoading(false)
-              console.log('response--->',response.doc)
-              if(response.doc.results.length>0){
-                setconcordanceResults(response.doc.results)
-                setShowConcordance(true)
-              }else{
-                alert('No results found for word: '+abc)
-              }
-  
-              // setData(response.doc);
-            }
-  
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
+    // if (abc.length > 0) {
+    //   fetch(`${url}/corpus/findKWIC/1`, {
+    //     method: "POST",
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Accept': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       word: abc,
+    //       criteria: 'all'
+    //     })
+    //   }).then(res => res.json())
+    //     .then((response) => {
+
+    //       // console.log('Data received search word --->', response);
+    //       // after
+    //       if (response.message === 'Success') {
+    //         // setIsLoading(false)
+    //         console.log('response--->', response.doc)
+    //         if (response.doc.results.length > 0) {
+    //           setconcordanceResults(response.doc.results)
+    //           setShowConcordance(true)
+    //         } else {
+    //           alert('No results found for word: ' + abc)
+    //         }
+
+    //         // setData(response.doc);
+    //       }
+
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //     });
+    // }
   }
-  useEffect(()=>{
+  useEffect(() => {
     // console.log(loction.state?.value)
     // console.log('idsfsdfi')
     setWord(loction.state?.value)
     if (Word === 'Gohar') {
       // console.log('uhrsfdiuhb ---> ',loction.state?.value)
       setWordDescription('noun')
-      setTimeout(()=>{
+      setTimeout(() => {
         searchConcordance()
         setShowConcordance(true)
-      },1000)
+      }, 1000)
     }
-    else if (Word === 'Azaan') {
-      setWordDescription('noun')
+    else if (Word === 'Choudary') {
+      let arr = ['File ID: MA-JN-031', 'Link: https://www.thenews.com.pk/magazine/money-matters/967492-the-ultimate-challenge'
+        , 'Concordance: the famous ‘Choudhry ‘of Gujrat', 'Usage in sentence: This matter is discussed by Choudary. Choudary drives them all to the site.',
+        'Meaning: A choudary is someone who is head of a tribe or village.', 'Grammatical category: Adjective',
+        'Word forms: plural Chudries chodriyon',
+        'Spelling variations: choudary, chodry']
+      setdescriptions(arr)
       searchConcordance()
       setShowConcordance(true)
     }
-    else if (Word === 'Betiyan') {
-      setWordDescription('Beti-yan noun plural')
+    else if (Word === 'Wahhabism') {
+      let arr = ['File ID: MA-JL-040', 'Link: https://www.dawn.com/news/1700077/smokers-corner-slurred-speech',
+        'Concordance: Wahhabism is often associated with', 'Usage in sentence: People talk less about Wahhabism. Wahhabism is more in practice in some areas than other school of thoughts.',
+        'Meaning : a sect of Muslims', 'Grammatical category : noun', 'Word forms: Wahhaby, Wahabbies, wahabi-yon', 'Possible Spelling variations : Wahhaby, wahhabee']
+      setdescriptions(arr)
       searchConcordance()
       setShowConcordance(true)
     }
-    else if (Word === 'Dadis') {
-      setWordDescription('Dadi-s   noun plural')
+    else if (Word === 'dera') {
+      let arr = ['File ID: BLGPK001', 'Link: https://blogpakistan.pk/category/entertainment/',
+        'Concordance: An eatery named ‘Dera’ provides refreshing and delicious food', 'Usage in sentence: there were many women on Nawab’s dera.',
+        'Meaning : camp, mound or settlement', 'Grammatical category: Noun ', 'Word forms: dera, deray (plural)',
+        'Possible Spelling variations : derah']
+      setdescriptions(arr)
       searchConcordance()
       setShowConcordance(true)
     }
-    else if (Word === 'Kaala') {
-      setWordDescription('adjective')
+    else if (Word === 'besan') {
+      let arr = ['File ID: 8YNG', 'Link: https://www.dawn.com/news/1696655/the-ultimate-sunscreen-guide',
+        'Concordance: Besan is a natural cleanser ', 'Usage in sentence: Besan is traditionally used in several Pakistani recipes.',
+        'Meaning: gram flour', 'Grammatical category: noun', 'Word forms: besun', 'Possible Spelling variations: Daadee (singular)']
+      setdescriptions(arr)
+      // setWordDescription('adjective')
+      // searchConcordance()
+      setShowConcordance(true)
+    }
+    else if (Word === 'shias and sunnis') {
+      let arr = ['File ID: CH1HF', 'Concordance : when people talk about the enmity between shias and sunnis',
+        'Usage in sentence: This conference is organized for both shia and sunni scholars.', 'Meaning : Two muslim sects ',
+        'Grammatical category : Noun', 'Word forms: Shiah, sunnee', 'Possible Spelling variations : Shiah and sunnee']
+      setdescriptions(arr)
+      // searchConcordance()
+      setShowConcordance(true)
+    }
+    else if (Word === 'chappatis') {
+      let arr = ['File ID: SRBRD', 'Link: Nadeem Aslam season of rain birds', 'Concordance: got ready to bake chappatis.',
+        'Usage in sentence: She made round chappatis for the dinner.', 'Meaning : Indian flat bread.',
+        'Grammatical category : Noun', 'Word forms: Chapati', 'Possible Spelling variations : Chapatee, Chapaty']
+      setdescriptions(arr)
+      // searchConcordance()
+      setShowConcordance(true)
+    }
+    else if (Word === 'ahl-e-hawas') {
+      let arr = ['File ID: PHDHEC003', 'Link: http://prr.hec.gov.pk/jspui/handle/123456789/16263',
+        'Concordance Taliban are ahl-ehawas', 'Usage in sentence: This gathering is not for ahl-e-hawas.',
+        'Meaning : men of greed', 'Grammatical category: Noun', 'Word forms: NA', 'Possible Spelling variations: Ahl-e-hawus']
+      setdescriptions(arr)
+      // searchConcordance()
+      setShowConcordance(true)
+    }
+    else if (Word === 'hijab') {
+      let arr = ['File ID: PHDHEC003', 'Link: http://prr.hec.gov.pk/jspui/handle/123456789/16263',
+        'Concordance : concept of hijab and physical intimacy', 'Usage in sentence: I like your hijab.',
+        'Meaning : clothing that covers head and/or face of women (muslim)', 'Grammatical category : Noun',
+        'Word forms: hijaab', 'Possible Spelling variations : Hejab, hijaab']
+      setdescriptions(arr)
+      // searchConcordance()
+      setShowConcordance(true)
+    }
+    else if (Word === 'imam') {
+      let arr = ['File ID: PHDHEC004', 'Link: http://prr.hec.gov.pk/jspui/handle/123456789/14436', 'Concordance : merciful followers of a faith…….Imam',
+        'Usage in sentence: Imam is now in the masjid.', 'Meaning : A person who is being followed while offering  prayer. He stands in front of everyone.', 'Grammatical category : Noun',
+        'Word forms: imams , Imambargah', 'Possible Spelling variations:  imaam']
+      setdescriptions(arr)
+      // searchConcordance()
+      setShowConcordance(true)
+    }
+    else if (Word === 'Jihadi') {
+      let arr = ['File ID: PHDHEC004', 'Link: http://prr.hec.gov.pk/jspui/handle/123456789/14436',
+        'Concordance: merciful followers of faith……jihadi ', 'Usage in sentence: His son in law is a jihadi.',
+        'Meaning:  Who is at war against non-muslims.', 'Grammatical category: Adjective',
+        'Word forms: jihad,Jihadi,jihadis, jihadist,jihadists', 'Possible Spelling variations: Jihaadi, Jihady,jihadee']
+      setdescriptions(arr)
+      // searchConcordance()
+      setShowConcordance(true)
+    }
+    else {
+      let arr = ['No description found for this word']
+      setdescriptions(arr)
       searchConcordance()
       setShowConcordance(true)
     }
-    else if (Word === 'Sarees') {
-      setWordDescription('Saree-s noun plural')
-      searchConcordance()
-      setShowConcordance(true)
-    }
-    else{
-      setWordDescription("POS not added for this word")
-      searchConcordance()
-      setShowConcordance(true)
-    }
-  },null)
+  }, null)
   return (
     <div>
-      <Bar/>
+      <Bar />
       <div className="container">
-        
+
         <br />
-        <button onClick={e=>{
+        <button onClick={e => {
           e.preventDefault()
           navigation('/Search')
-        }} className="rounded border text-white form-control bg-primary" style={{width:'100px'}}>
-                  Go Back
-                </button>
-                <br/>
+        }} className="rounded border text-white form-control bg-primary" style={{ width: '100px' }}>
+          Go Back
+        </button>
+        <br />
         <div className="container row">
           &nbsp;
           <div style={{ color: "#b03e41" }}>
             <h4>Definition of '{Word}'</h4>
           </div>
-          <div className="float-start col-md-4 col-sm-4">
+          <div className="float-start col-md-10 col-sm-10">
             <br />
             <h2>{Word}
-            {"    "}
-            <span>
-            <i
-              class="fa fa-volume-up fa-lg"
-              
-              style={{ color: "#b03e41" }}
-              aria-hidden="true"
-            ></i>{" "}
-            &nbsp;{" "}
-            <i
-              class="fa fa-info-circle fa-lg"
-              aria-hidden="true"
-              style={{ color: "grey" }}
-            ></i>
-            
-          </span>
-          <br/>
-          <span style={{ color: "#b03e41" }}>
-            <strong>{wordDescription}</strong>
-          </span>
+              {"    "}
+              <span>
+                <i
+                  class="fa fa-volume-up fa-lg"
+
+                  style={{ color: "#b03e41" }}
+                  aria-hidden="true"
+                ></i>{" "}
+                &nbsp;{" "}
+                <i
+                  class="fa fa-info-circle fa-lg"
+                  aria-hidden="true"
+                  style={{ color: "grey" }}
+                ></i>
+
+              </span>
+              <br />
+              <span>
+                {descriptions.length > 0 && descriptions.map((desc) => <p>
+                  <strong><span style={{ color: "#b03e41" }}>{desc.split(':')[0]}:</span></strong><span>{desc.split(':')[1]}</span></p>)}
+              </span>
             </h2>
-            
+
             {/* <p>
               <span>
                 <strong>Collins</strong>
@@ -145,7 +209,7 @@ export default function List() {
           &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
           &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
           &nbsp; &nbsp; &nbsp;
-          <div className="float-end col-md-4 col-sm-4">
+          {/* <div className="float-end col-md-4 col-sm-4">
             <br />
             <h6>
               Word Frequency &nbsp;
@@ -157,11 +221,11 @@ export default function List() {
             </h6>
 
             <i class="fa fa-share-alt-square  fa-3x"></i>
-          </div>
+          </div> */}
         </div>
         <div>
           {/* &nbsp; &nbsp; &nbsp; Words forms: plural hunch backs &nbsp;{" "} */}
-          
+
           <br />
           <br />
           &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;{" "}
@@ -180,29 +244,29 @@ export default function List() {
             <strong>[Offensive, old-fashioned]</strong>{" "}
           </span> */}
         </div>
-        {showConcordance===true && <table class="table">
-                <thead>
-                  <h2 style={{textAlign:"center"}}>Concordance</h2>
-                  <tr>
-                    <th scope="col">Text From File</th>
-                    <th scope="col">Left Text</th>
-                    <th scope="col">Center Word</th>
-                    <th scope="col">Right Text</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {concordanceResults.length>0 && concordanceResults.map((item,index)=><tr>
-                    <th scope="row">{item.filename}</th>
-                    <td>{item.preText}</td>
-                    <td>{Word}</td>
-                    <td>{item.postText}</td>
-                  </tr>)}
+        {showConcordance === true && <table class="table">
+          <thead>
+            <h2 style={{ textAlign: "center" }}>Concordance</h2>
+            <tr>
+              <th scope="col">Text From File</th>
+              <th scope="col">Left Text</th>
+              <th scope="col">Center Word</th>
+              <th scope="col">Right Text</th>
+            </tr>
+          </thead>
+          <tbody>
+            {concordanceResults.length > 0 && concordanceResults.map((item, index) => <tr>
+              <th scope="row">{item.filename}</th>
+              <td>{item.preText}</td>
+              <td>{Word}</td>
+              <td>{item.postText}</td>
+            </tr>)}
 
-                </tbody>
-              </table>}
+          </tbody>
+        </table>}
         <br />
         <span style={{ color: "#b03e41" }}>
-          PakLocCorp. Copyrights &copy; pakloccorp.com 
+          PakLocCorp. Copyrights &copy; pakloccorp.com
         </span>
       </div>
     </div>
